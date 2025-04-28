747f345798832385d07ceadd328456cfc4d431b1
Ahmed Šabanović <92882375+AhmedSabanovic@users.noreply.github.com>
2025-04-28 21:05:43 +0200

    Update merge2.py

--- merge2.py
+++ merge2.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # Load the datasets
-ascat_soil_categories_path = r"c:\Users\ahmed\Desktop\GEO Test\ascat_soil_categories.csv"
-aggregated_merged_path = r"c:\Users\ahmed\Desktop\GEO Test\cell1248_predictions_baseline.csv"
+ascat_soil_categories_path = r"ascat_soil_categories.csv"
+aggregated_merged_path = r"cell1248_predictions_baseline.csv"
 
 ascat_soil_categories = pd.read_csv(ascat_soil_categories_path)
 aggregated_merged = pd.read_csv(aggregated_merged_path)
@@ -15,7 +15,7 @@ if 'soil_cat' in aggregated_merged.columns:
 merged_data = pd.merge(ascat_soil_categories, aggregated_merged, on="gpi_ascat", how="inner")
 
 # Save the merged dataset to a new CSV file
-output_path = r"c:\Users\ahmed\Desktop\GEO Test\ascat_merged_baseline.csv"
+output_path = r"ascat_merged_baseline.csv"
 merged_data.to_csv(output_path, index=False)
 
 print(f"Merged data saved to {output_path}")
